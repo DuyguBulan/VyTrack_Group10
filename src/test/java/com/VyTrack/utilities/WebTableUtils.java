1f5444b4672ecca534206681038f48593e68290e
duygubulan <duygum_ay@hotmail.com>
2022-03-10 14:54:07 -0500

    Initial commit

---
+++ src/test/java/com/VyTrack/utilities/WebTableUtils.java
@@ -0,0 +1,19 @@
+package com.VyTrack.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WebTableUtils {
+
+    public static void waitTillLoaderMaskDisappear() {
+        try {
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
+            WebElement loaderMask = Driver.getDriver().findElement(By.cssSelector("div[class='loader-mask shown']"));
+            wait.until(ExpectedConditions.invisibilityOf(loaderMask));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
